91d3cf900daf78f0a8c379e2b32d3725249cafd1
ivant <ivant@DESKTOP-4MS89QH>
2025-07-15 19:53:19 +0300

    Add project files.

---
+++ BMWDealership_SoftUniFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMWDealership_SoftUniFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
