fa8a1ac1ed30b2154fbf95386624b882cccafbcb
Ivan Terziev <ivanterziev19999@gmail.com>
2025-08-09 19:27:11 +0300

    FAQ and Contact Integration + Some changes

---
+++ BMWDealership_SoftUniFinal/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(string Name, string Email, string Message)
+    {
+        ViewBag.Confirmation = $"Thanks for your message, {Name}! We'll reply soon.";
+
+        return View();
+    }
+}
